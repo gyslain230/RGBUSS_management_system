f5e0d91acb8962d538cb80b71200a0f806107d38
rgyslain <gislainrugira@gmail.com>
2025-08-10 18:47:04 +0300

    removing historyapifallback

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
   },
   server: {
-    historyApiFallback: {
-      index: '/index.html',
-    },
+    // No need for historyApiFallback
   },
   preview: {
-    historyApiFallback: {
-      index: '/index.html',
-    },
+    // No need for historyApiFallback
   },
 });
