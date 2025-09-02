8d6603db37311a1fe78b333a114bf9e32a3ba420
Gyslain <110432020+gyslain230@users.noreply.github.com>
2025-09-02 13:03:32 +0300

    Updated UserManagement.tsx

--- src/pages/UserManagement.tsx
+++ src/pages/UserManagement.tsx
@@ -84,7 +84,7 @@ const UserManagement = () => {
     return (
       <div className="text-center py-12 dark:bg-gray-900 min-h-screen flex items-center justify-center">
         <div>
-        <Shield className="h-12 w-12 text-gray-400 mx-auto mb-4" />
+          <Shield className="h-12 w-12 text-gray-400 mx-auto mb-4" />
           <h3 className="text-lg font-medium text-gray-900 dark:text-white mb-2">Access Denied</h3>
           <p className="text-gray-600 dark:text-gray-400">You don't have permission to access this page.</p>
         </div>
@@ -247,3 +247,6 @@ const UserManagement = () => {
       </div>
     </div>
   );
+};
+
+export default UserManagement;
