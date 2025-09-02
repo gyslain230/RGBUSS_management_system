841dbfb93a758235bb2d8cb53c910bc17278de2b
Gyslain <110432020+gyslain230@users.noreply.github.com>
2025-09-02 14:16:16 +0300

    Fix clearCache import error in Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Bell, LogOut, Mail, Clock, Shield, Menu } from 'lucide-react';
 import { useAuth } from '../contexts/AuthContext';
+import { clearCache } from '../utils/security';
 import ThemeToggle from './ThemeToggle';
 
 interface HeaderProps {
